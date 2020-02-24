cbf0c03fbb1bb4d0cb286372c8e22593b9d46616
lmanuelprodeco <lmanuelmb.prodeco@gmail.com>
2020-02-23 23:27:17 -0600

    commit 2020-02-21 23:00

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
 
 	path('',include(('bases.urls','bases'), namespace='bases')),
     path('inv/',include(('inv.urls','inv'), namespace='inv')),
+    path('cmp/',include(('cmp.urls','cmp'), namespace='cmp')),
 	
     path('admin/', admin.site.urls),
 ]
