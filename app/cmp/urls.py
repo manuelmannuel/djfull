bc7239e4d4cff6a0e57faf56af5f98b5e864dea0
crediweb <lmanuelmb.prodeco@gmail.com>
2020-02-27 16:42:36 -0600

    work 2020-02-27 17:00

--- app/cmp/urls.py
+++ app/cmp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 from .views import ProveedorView, ProveedorNew, ProveedorEdit, \
-	proveedorInactivar
+	proveedorInactivar, \
+	ComprasView, compras
 
 urlpatterns = [
 	path('proveedores/',ProveedorView.as_view(), name='proveedor_list'),
 	path('proveedores/new',ProveedorNew.as_view(), name='proveedor_new'),
 	path('proveedores/edit/<int:pk>',ProveedorEdit.as_view(), name='proveedor_edit'),
 	path('proveedores/inactivar/<int:id>',proveedorInactivar, name='proveedor_inactivar'),
+
+	path('compras/',ComprasView.as_view(), name='compras_list'),
+	path('compras/new',compras, name='compras_new'),
 ]
--- app/cmp/views.py
+++ app/cmp/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+import datetime
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 import json
 from django.contrib.auth.decorators import login_required, permission_required
 
-from .models import Proveedor
-from cmp.forms import ProveedorForm
+from .models import Proveedor, ComprasEnc, ComprasDet
+from cmp.forms import ProveedorForm, ComprasEncForm
 
 from bases.views import SinPrivilegios
+from inv.models import Producto
 
 
 class ProveedorView(SinPrivilegios, generic.ListView):
@@ -65,3 +67,43 @@ def proveedorInactivar(request,id):
 		return HttpResponse('Proveedor Inactivado')
 
 	return render(request,template_name,contexto)		
+
+class ComprasView(SinPrivilegios, generic.ListView):
+	permission_required = "cmp.view_compras"
+	model = ComprasEnc
+	template_name = "cmp/compras_list.html"
+	context_object_name = 'obj'	
+
+@login_required(login_url='/login/')
+@permission_required('cmp.view_comprasenc', login_url='bases:sin_privilegios')
+def compras(request,compra_id=None):
+	template_name="cmp/compras.html"
+	prod=Producto.objects.filter(estado=True)
+	form_compras={}
+	contexto={}
+
+	if request.method=='GET':
+		form_compras=ComprasEncForm()
+		enc=ComprasEnc.objects.filter(pk=compra_id).first()
+
+		if enc:
+			det=ComprasDet.objects.filter(compras=enc)
+			fecha_compra=datetime.date.isoformat(enc.fecha_compra)
+			fecha_factura=datetime.date.isoformat(enc.fecha_factura)
+			e={
+				'fecha_compra':fecha_compra,
+				'proveedor':enc.proveedor,
+				'observacion':enc.observacion,
+				'no_factura':enc.no_factura,
+				'fecha_factura':fecha_factura,
+				'sub_total':enc.sub_total,
+				'descuento':enc.descuento,
+				'total':enc.total
+			}
+
+			form_compras=ComprasEncForm(e)
+		else:
+			det=None
+
+		contexto={'productos':prod,'encabezado':enc,'detalle':det,'form_enc':form_compras}	
+		return render(request, template_name, contexto)
