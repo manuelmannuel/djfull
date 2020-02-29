43d239a8ccb18cd763c75efac6f1051289c7e244
crediweb <lmanuelmb.prodeco@gmail.com>
2020-02-29 11:16:37 -0600

    work 2020-02-29 11:00

--- app/cmp/views.py
+++ app/cmp/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import generic
 from django.urls import reverse_lazy
 import datetime
+from django.http import HttpResponse, JsonResponse
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 import json
+from django.db.models import Sum
 from django.contrib.auth.decorators import login_required, permission_required
 
 from .models import Proveedor, ComprasEnc, ComprasDet
@@ -87,7 +89,7 @@ def compras(request,compra_id=None):
 		enc=ComprasEnc.objects.filter(pk=compra_id).first()
 
 		if enc:
-			det=ComprasDet.objects.filter(compras=enc)
+			det=ComprasDet.objects.filter(compra=enc)
 			fecha_compra=datetime.date.isoformat(enc.fecha_compra)
 			fecha_factura=datetime.date.isoformat(enc.fecha_factura)
 			e={
@@ -106,4 +108,72 @@ def compras(request,compra_id=None):
 			det=None
 
 		contexto={'productos':prod,'encabezado':enc,'detalle':det,'form_enc':form_compras}	
-		return render(request, template_name, contexto)
+
+	if request.method=='POST':
+		fecha_compra=request.POST.get("fecha_compra")
+		observacion=request.POST.get("observacion")
+		no_factura=request.POST.get("no_factura")
+		fecha_factura=request.POST.get("fecha_factura")
+		proveedor=request.POST.get("proveedor")
+		sub_total=0
+		descuento=0
+		total=0
+
+		if not compra_id:
+			prov=Proveedor.objects.get(pk=proveedor)
+
+			enc=ComprasEnc(
+				fecha_compra=fecha_compra,
+				observacion=observacion,
+				no_factura=no_factura,
+				fecha_factura=fecha_factura,
+				proveedor=prov,
+				uc=request.user
+			)
+			if enc:
+				enc.save()
+				compra_id=enc.id
+		else:
+			enc=ComprasEnc.objects.filter(pk=compra_id).first()
+			if enc:
+				enc.fecha_compra=fecha_compra
+				enc.observacion=observacion
+				enc.no_factura=no_factura
+				enc.fecha_factura=fecha_factura
+				enc.um=request.user.id
+				enc.save()
+
+		if not compra_id:
+			return redirect("cmp:compras_list")	
+
+		producto=request.POST.get("id_id_producto")
+		cantidad=request.POST.get("id_cantidad_detalle")
+		precio=request.POST.get("id_precio_detalle")
+		sub_total_detalle=request.POST.get("id_subtotal_detalle")
+		descuento_detalle=request.POST.get("id_descuento_detalle")
+		total_detalle=request.POST.get("id_total_detalle")
+
+		prod=Producto.objects.get(pk=producto)				
+
+		det=ComprasDet(
+			compra=enc,
+			producto=prod,
+			cantidad=cantidad,
+			precio_prv=precio,
+			descuento=descuento_detalle,
+			costo=0,
+			uc=request.user
+		)
+
+		if det:
+			det.save()
+
+			sub_total=ComprasDet.objects.filter(compra=compra_id).aggregate(Sum('sub_total'))
+			descuento=ComprasDet.objects.filter(compra=compra_id).aggregate(Sum('descuento'))
+			enc.sub_total=sub_total["sub_total__sum"]
+			enc.descuento=descuento["descuento__sum"]
+			enc.save()
+
+		return redirect("cmp:compras_edit",compra_id=compra_id)
+
+	return render(request, template_name, contexto)
