358cd9dbbaabe970a6eba44c45e8e195b2b856da
crediweb <lmanuelmb.prodeco@gmail.com>
2020-03-03 15:14:43 -0600

    work 2020-03-03 15:00

--- app/cmp/models.py
+++ app/cmp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 #Para los signals
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.db.models import Sum
 
 from bases.models import ClaseModelo
 from inv.models import Producto
@@ -39,27 +40,27 @@ class Proveedor(ClaseModelo):
 		verbose_name_plural="Proveedores"	
 
 class ComprasEnc(ClaseModelo):
-	fecha_compra=models.DateField(null=True,blank=True)
-	observacion=models.TextField(blank=True,null=True)
-	no_factura=models.CharField(max_length=100)
-	fecha_factura=models.DateField()
-	sub_total=models.FloatField(default=0)
-	descuento=models.FloatField(default=0)
-	total=models.FloatField(default=0)
+    fecha_compra=models.DateField(null=True,blank=True)
+    observacion=models.TextField(blank=True,null=True)
+    no_factura=models.CharField(max_length=100)
+    fecha_factura=models.DateField()
+    sub_total=models.FloatField(default=0)
+    descuento=models.FloatField(default=0)
+    total=models.FloatField(default=0)
 
-	proveedor=models.ForeignKey(Proveedor,on_delete=models.CASCADE)
+    proveedor=models.ForeignKey(Proveedor,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return '{}'.format(self.observacion)
 
-	def __str__(self):
-		return '{}'.format(self.observacion)		
+    def save(self):
+        self.observacion = self.observacion.upper()
+        self.total = self.sub_total - self.descuento
+        super(ComprasEnc,self).save()
 
-	def save(self):
-		self.observacion=self.observacion.upper()
-		self.total=self.sub_total - self.descuento
-		super(ComprasEnc,self).save()
-
-	class Meta:
-		verbose_name_plural="Encabezado Compras"
-		verbose_name="Encabezado Compra"	
+    class Meta:
+        verbose_name_plural = "Encabezado Compras"
+        verbose_name="Encabezado Compra"	
 
 class ComprasDet(ClaseModelo):
 	compra=models.ForeignKey(ComprasEnc,on_delete=models.CASCADE)
@@ -98,6 +99,18 @@ def detalle_compra_borrar(sender,instance, **kwargs):
 
 	prod=Producto.objects.filter(pk=id_producto).first()
 	if prod:
-		cantidad=int(prod.exixtencia)-int(instance.cantidad)
-		prod.exixtencia=cantidad
+		cantidad=int(prod.existencia)-int(instance.cantidad)
+		prod.existencia=cantidad
+		prod.save()
+
+@receiver(post_save, sender=ComprasDet)
+def detalle_compra_guardar(sender,instance,**kwargs):
+	id_producto=instance.producto.id
+	fecha_compra=instance.compra.fecha_compra
+
+	prod=Producto.objects.filter(pk=id_producto).first()
+	if prod:
+		cantidad=int(prod.existencia) + int(instance.cantidad)
+		prod.existencia=cantidad
+		prod.ultima_compra=fecha_compra
 		prod.save()
