104574e73ed8e5e6b2aac1bac7bf3df719246ac2
crediweb <lmanuelmb.prodeco@gmail.com>
2020-03-04 17:16:08 -0600

    work 2020-03-04 17:00

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'bases',
     'inv',
     'cmp',
+    'fac',
+    'django_userforeignkey',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_userforeignkey.middleware.UserForeignKeyMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
---
+++ app/fac/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render,redirect
+from django.views import generic
+
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponse
+from datetime import datetime
+from django.contrib import messages
+
+from django.contrib.auth import authenticate
+
+from bases.views import SinPrivilegios
+
+from .models import Cliente, FacturaEnc, FacturaDet
+from .forms import ClienteForm
+
+class ClienteView(SinPrivilegios, generic.ListView):
+	permission_required = "fac.view_cliente"
+	model = Cliente
+	template_name = "fac/cliente_list.html"
+	context_object_name = 'obj'
+
+class VistaBaseCreate(SuccessMessageMixin,SinPrivilegios, \
+	generic.CreateView):
+	context_object_name='obj'
+	success_message="Registro Agregado Satisfactoriamente"
+
+	def form_valid(self,form):
+		form.instance.uc=self.request.user
+		return super().form_valid(form)
+
+class VistaBaseEdit(SuccessMessageMixin,SinPrivilegios, \
+	generic.UpdateView):
+	context_object_name='obj'
+	success_message="Registro Actualizado Satisfactoriamente"
+
+	def form_valid(self,form):
+		form.instance.um=self.request.user.id
+		return super().form_valid(form)
+
+class ClienteNew(VistaBaseCreate):
+	model=Cliente
+	template_name="fac/cliente_form.html"
+	form_class=ClienteForm		
+	success_url=reverse_lazy("fac:cliente_list")
+	permission_required="fac.add_cliente"
+
+class ClienteEdit(VistaBaseEdit):
+	model=Cliente
+	template_name="fac/cliente_form.html"
+	form_class=ClienteForm		
+	success_url=reverse_lazy("fac:cliente_list")
+	permission_required="fac.change_cliente"	
+
+@login_required(login_url="/login/")
+@permission_required("fac.change_cliente",login_url="/login/")
+def clienteInactivar(request,id):
+	cliente=Cliente.objects.filter(pk=id).first()
+
+	if request.method=="POST":
+		if cliente:
+			cliente.estado = not cliente.estado
+			cliente.save()
+			return HttpResponse("OK")
+		return HttpResponse("FAIL")		
+
+	return HttpResponse("FAIL")	
+
+class FacturaView(SinPrivilegios,generic.ListView):
+	model=FacturaEnc
+	template_name="fac/factura_list.html"
+	context_object_name="obj"
+	permission_required="fac.view_facturaenc"	
+
+@login_required(login_url='/login/')
+@permission_required('fac.change_facturaenc', login_url='bases:sin_privilegios')
+def facturas(request,id=None):
+    template_name='fac/facturas.html'
+    contexto = {}
+    return render(request,template_name,contexto)
+
+
